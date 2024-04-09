3613537d668ece9d50c899f365de1a122a4f9af2
Stefana <stefanachiritescu@yahoo.com>
2024-04-09 21:59:35 +0100

    - make tabview swipe to planet that is being clcked on from overview tab

--- Astrologion/Core/Transits/View/TransitsTabView.swift
+++ Astrologion/Core/Transits/View/TransitsTabView.swift
@@ -5,33 +5,40 @@ struct TransitsTabView: View {
 
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 20) {
-                ForEach(TransitTab.allCases, id: \.self) { tab in
-                    VStack {
-                        Button(action: {
-                            withAnimation {
-                                self.selectedTab = tab
+            ScrollViewReader { scrollViewProxy in
+                HStack(spacing: 20) {
+                    ForEach(TransitTab.allCases, id: \.self) { tab in
+                        VStack {
+                            Button(action: {
+                                withAnimation {
+                                    self.selectedTab = tab
+                                }
+                            }) {
+                                Text(tab.title)
+                                    .font(.system(size: 16))
+                                    .foregroundColor(self.selectedTab == tab ? .black : .gray)
+                                    .fontWeight(self.selectedTab == tab ? .bold : .regular)
+                                    .padding(.vertical, 8)
                             }
-                        }) {
-                            Text(tab.title)
-                                .font(.system(size: 16)) 
-                                .foregroundColor(self.selectedTab == tab ? .black : .gray)
-                                .fontWeight(self.selectedTab == tab ? .bold : .regular)
-                                .padding(.vertical, 8) 
+                            .id(tab)
+
+                            if selectedTab == tab {
+                                Rectangle()
+                                    .frame(height: 2)
+                                    .foregroundColor(.black)
+                                    .transition(.opacity)
+                            }
                         }
-                        
-                        // underline selected tab
-                        if selectedTab == tab {
-                            Rectangle()
-                                .frame(height: 2)
-                                .foregroundColor(.black)
-                                .transition(.opacity)
-                        }
+                        .frame(minWidth: 100, maxWidth: .infinity)
                     }
-                    .frame(minWidth: 100, maxWidth: .infinity)
+                }
+                .padding(.horizontal)
+                .onChange(of: selectedTab) { newValue in
+                    withAnimation {
+                        scrollViewProxy.scrollTo(newValue, anchor: .leading)
+                    }
                 }
             }
-            .padding(.horizontal)
         }
         .background(Color.white)
     }
